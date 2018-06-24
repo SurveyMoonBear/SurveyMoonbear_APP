5d703978eda403c53c0296e73a99197e07365f98
LetitiaShe <wehuin1215@gmail.com>
2018-06-24 18:59:21 +0800

    start_flag default value

--- infrastructure/database/migrations/002_surveys_create.rb
+++ infrastructure/database/migrations/002_surveys_create.rb
@@ -8,7 +8,7 @@ Sequel.migration do
       foreign_key :owner_id, :accounts
       String      :origin_id, unique: true
       String      :title
-      TrueClass   :start_flag, default: 0
+      TrueClass   :start_flag, default: false
     end
   end
 end
