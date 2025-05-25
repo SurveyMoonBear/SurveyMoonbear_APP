b69eace85ecd938f6ec21cfd4dbe8022009e3541
MING-CHUNLee <mindy80230@gmail.com>
2025-05-25 17:31:15 +0800

    fix(db): ensure wipe_database cleans all table

--- spec/helpers/database_helper.rb
+++ spec/helpers/database_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
-
+require_relative '../../init'
 # To clean database during test runs
 class DatabaseHelper
   def self.wipe_database
     SurveyMoonbear::App.DB.run('PRAGMA foreign_keys = OFF')
     SurveyMoonbear::Database::AccountOrm.map(&:destroy)
+    SurveyMoonbear::Database::AccountSurveysOrm.map(&:destroy)
+    SurveyMoonbear::Database::StudyOrm.map(&:destroy)
+    SurveyMoonbear::Database::ParticipantOrm.map(&:destroy)
+    SurveyMoonbear::Database::NotificationOrm.map(&:destroy)
     SurveyMoonbear::Database::SurveyOrm.map(&:destroy)
     SurveyMoonbear::Database::PageOrm.map(&:destroy)
     SurveyMoonbear::Database::ItemOrm.map(&:destroy)
