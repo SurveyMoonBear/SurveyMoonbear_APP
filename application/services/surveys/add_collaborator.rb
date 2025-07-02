7bcb36aafcd053a01e82c9db9d4a920656a65941
MING-CHUNLee <mindy80230@gmail.com>
2025-07-02 14:08:15 +0800

    chore: refactor AddCollaborator to reduce RuboCop ABC size

--- application/services/surveys/add_collaborator.rb
+++ application/services/surveys/add_collaborator.rb
@@ -3,53 +3,76 @@
 require 'dry/monads'
 require_relative '../../policies/survey_policy'
 
-
 module SurveyMoonbear
   module Service
+    # Service to add a collaborator to a survey
     class AddCollaborator
       include Dry::Monads[:result]
 
+      # Custom error for when a user is not allowed to add collaborators
       class ForbiddenError < StandardError
         def message = 'You are not allowed to add collaborators to this survey.'
       end
 
+      # Custom error for when a collaborator is not found
       class NotFoundError < StandardError
         def message = 'Collaborator not found in system.'
       end
 
       def call(account:, survey_id:, collaborator_email:)
-        
-        raise NotFoundError unless survey = Repository::Surveys.find_id(survey_id)
-        account = Repository::Accounts.find_id(account['id'])
-        role    = Repository::AccountSurveys.find_role(account.id, survey.id)
+        survey       = find_survey!(survey_id)
+        actor        = find_account!(account['id'])
+        authorize_add!(actor, survey)
+
+        collaborator = find_collaborator!(collaborator_email)
+        authorize_collaborator!(collaborator, survey)
+
+        add_collaborator!(collaborator, survey_id)
+      rescue ForbiddenError, NotFoundError => e
+        Failure(e.message)
+      rescue StandardError
+        Failure('Unexpected server error while adding collaborator.')
+      end
+
+      private
+
+      def find_survey!(survey_id)
+        Repository::Surveys.find_id(survey_id) ||
+          (raise NotFoundError)
+      end
+
+      def find_account!(account_id)
+        Repository::Accounts.find_id(account_id) ||
+          (raise NotFoundError)
+      end
+
+      def authorize_add!(account, survey)
+        role   = Repository::AccountSurveys.find_role(account.id, survey.id)
         policy = SurveysPolicy.new(account, survey, role)
-
         raise ForbiddenError unless policy.can_add_collaborators?
+      end
 
-        collaborator = Repository::Accounts.find_email(collaborator_email)
-        raise NotFoundError unless collaborator
+      def find_collaborator!(email)
+        Repository::Accounts.find_email(email) ||
+          (raise NotFoundError)
+      end
 
-        collaborator_role = Repository::AccountSurveys.find_role(collaborator.id, survey_id)
+      def authorize_collaborator!(collaborator, survey)
+        role   = Repository::AccountSurveys.find_role(collaborator.id, survey.id)
+        policy = SurveysPolicy.new(collaborator, survey, role)
+        raise ForbiddenError unless policy.can_collaborate?
+      end
 
-        policy_for_collaborator = SurveysPolicy.new(collaborator, survey, collaborator_role)
-        return Failure('Already a collaborator')  unless  policy_for_collaborator.can_collaborate?
-
-        new_relation = Entity::AccountSurvey.new(
+      def add_collaborator!(collaborator, survey_id)
+        relation = Entity::AccountSurvey.new(
           owner_id: collaborator.id,
           survey_id: survey_id,
           role: 'collaborator',
           created_at: Time.now,
           updated_at: Time.now
         )
-
-        Repository::AccountSurveys.create(new_relation)
-
+        Repository::AccountSurveys.create(relation)
         Success("#{collaborator.username} was added as collaborator.")
-      rescue ForbiddenError, NotFoundError => e
-        Failure(e.message)
-      rescue StandardError => e
-        puts "AddCollaborator Error: #{e.inspect}"
-        Failure('Unexpected server error while adding collaborator.')
       end
     end
   end
